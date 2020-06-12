b2a26fe4176c598b02235cc723da81e500eb0274
felipe <domfelipe.moraes@gmail.com>
2020-06-12 16:08:02 -0300

    base game

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var database = require('./database');
 database.connect(function(err_connect){});//Conecta a base de dados
 
 var PlayersOnlline = {};
+var Territorios = {};
 var roomFull = false;
 var quantPlayers = 0;
 io.on('connection', function(socket){
@@ -392,7 +393,7 @@ io.on('connection', function(socket){
   });//end socket.on(START_GAME)
 
   socket.on("SOLICITAR_DADOS", function(pack){
-    console.log("pegando dados");
+    //console.log("pegando dados");
     var solicitarDados = {
       idPlayerWant: pack.idWant,
       idPlayerGive: pack.idGive,
@@ -401,11 +402,11 @@ io.on('connection', function(socket){
 
 
     socket.broadcast.emit("PEGAR_DADOS",solicitarDados);
-    console.log("dados a serem enviados index" + solicitarDados.index);
+  //  console.log("dados a serem enviados index" + solicitarDados.index);
   });//end socket.on(REQUIRE_DADOS)
 
   socket.on("ENVIAR_DADOS", function(pack){
-    console.log("enviar dados index" + pack.index);
+    //console.log("enviar dados index" + pack.index);
     var enviarDados = {
       idPara: pack.idPara,
       idDe: pack.idDe,
@@ -421,7 +422,7 @@ io.on('connection', function(socket){
     }
 
     socket.broadcast.emit("DADOS_ENVIADOS",enviarDados);
-    console.log("ENVIANDO Dados");
+    //console.log("ENVIANDO Dados");
   });//end socket.on(REQUIRE_DADOS)
 
   socket.on("ENVIAR_DADOS_BATALHA", function(pack){
@@ -446,7 +447,7 @@ io.on('connection', function(socket){
       money : pack.money,
       bread : pack.bread
     }
-    console.log("ENVIANDO DADOS BATALHA PARA tds  por " + enviarDados.idDe);
+    //console.log("ENVIANDO DADOS BATALHA PARA tds  por " + enviarDados.idDe);
     socket.broadcast.emit("DADOS_ENVIADOS_BATALHA",enviarDados);
   });//end socket.on(REQUIRE_DADOS_BATALHA)
 
@@ -485,6 +486,52 @@ io.on('connection', function(socket){
     }
     socket.broadcast.emit("ACTION_MAP_RESULT", enviarDadosMap);
   });//end ACTION_MAP
+
+  socket.on("DADOS_CLIENTE", function(pack){
+    console.log(pack.index + " sendo modificado");
+    Territorios[pack.index] = {
+      index: pack.index,
+      tag: pack.tag,
+      tagUser: pack.tagUser,
+      nameUser: pack.nameUser,
+      IDuser: pack.IDuser,
+      nameTerritorio: pack.nameTerritorio,
+
+      a1: pack.a1,
+      a2: pack.a2,
+      d1: pack.d1,
+      d2: pack.d2,
+      attack: pack.attack,
+      defence: pack.defence,
+      money: pack.money,
+      bread: pack.bread,
+      levelFazenda: pack.levelFazenda,
+      levelInposto: pack.levelImposto,
+      levelArmazemComida: pack.levelArmazemComida,
+      levelArmazemMoeda: pack.levelArmazemMoeda,
+      levelEspiao: pack.levelEspiao,
+      tipoProducao: pack.tipoProducao,
+      bonusProducao: pack.bonusProducao,
+      decrescimoProducao: pack.decrescimoProducao
+    };
+    console.log(Territorios[pack.index].index +" foi modificado:");
+    console.log("nameTerritorio: " +Territorios[pack.index].nameTerritorio);
+    console.log("nameUser: " +Territorios[pack.index].nameUser);
+    console.log("tagUser: " +Territorios[pack.index].tagUser);
+    console.log("tag territorio: " +Territorios[pack.index].tag);
+  });//end dados clientes
+
+  socket.on("CLIENTE_SOLICITA_DADOS", function(pack){
+    console.log(" on CLIENTE_SOLICITA_DADOS");
+    if (Territorios[pack.index].index == pack.index){
+      console.log("enviando dados sobre o territorio " + Territorios[pack.index].index);
+      socket.emit("CLIENTE_RECEBE_DADOS", Territorio[pack.index]);
+    }
+
+
+  }); //end cliente_solicita_dados
+
+
   socket.on("disconnect", function(){
     roomFull = false;
     if (CurrentPlayer.state == "math" || CurrentPlayer.state == "lobby" || CurrentPlayer.state == "pre-lobby"){
